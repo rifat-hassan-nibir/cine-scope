512c42d7e8bfee166f149654989ba81ae27b85c9
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-04 20:32:28 +0600

    feat: rename and enhance the getPopularMoviesByGenres to accept different parameters

--- services/tmdb.ts
+++ services/tmdb.ts
@@ -1,3 +1,4 @@
+// get top rated movies
 export const getTopRatedMovies = async () => {
   const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/top_rated?language=en-US&page=1`, {
     method: "GET",
@@ -10,22 +11,37 @@ export const getTopRatedMovies = async () => {
   return data.results;
 };
 
-export const getPopularMoviesByGenres = async (genreId: number) => {
-  const res = await fetch(
-    `${process.env.TMDB_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`,
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-      },
+// get popular movies by genres
+export const getMoviesData = async (
+  genreId?: number,
+  sortBy: string = "popularity.desc",
+  page: number = 1,
+) => {
+  const params = new URLSearchParams({
+    include_adult: "false",
+    include_video: "false",
+    language: "en-US",
+    page: page.toString(),
+    sort_by: sortBy,
+  });
+
+  if (genreId && !isNaN(genreId)) {
+    params.append("with_genres", genreId.toString());
+  }
+
+  const res = await fetch(`${process.env.TMDB_BASE_URL}/discover/movie?${params.toString()}`, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
     },
-  );
+  });
   const data = await res.json();
-  return data.results;
+  return data.results || [];
 };
 
-export const getAllGenreId = async () => {
+// get all genre id
+export const getAllGenres = async () => {
   const res = await fetch(`${process.env.TMDB_BASE_URL}/genre/movie/list?language=en-US`, {
     method: "GET",
     headers: {
