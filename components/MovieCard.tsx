2af862a545b2a07e035b8bf649d20ad4e79d7fc1
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-06 12:46:17 +0600

    refactor: change movie card design

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,6 @@
 import { TMDB_IMAGE_BASE_URL } from "@/constants";
 import { Movie } from "@/types/tmdb";
-import { Plus, Star } from "lucide-react";
+import { Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,14 +19,9 @@ export default function MovieCard({ movie }: { movie: Movie }) {
           className="object-cover"
         />
 
-        {/* Hover Overlay */}
-        <div className="absolute inset-0 bg-linear-to-t from-black/90 via-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-end p-4">
+        <div className="absolute inset-0 bg-linear-to-t from-black/90 via-black/10 to-transparent flex flex-col justify-end p-4">
           <div className="transform translate-y-4 group-hover:translate-y-0 transition-transform duration-300">
             <div className="flex items-center gap-2 mb-2">
-              <span className="flex items-center text-yellow-400 text-xs font-bold">
-                <Star size={12} className="fill-yellow-400 mr-1" />
-                {movie.vote_average.toFixed(1)}
-              </span>
               <span className="text-gray-300 text-xs">
                 {new Date(movie.release_date).getFullYear()}
               </span>
@@ -35,15 +30,11 @@ export default function MovieCard({ movie }: { movie: Movie }) {
             <h3 className="text-white font-bold text-sm mb-3 line-clamp-2 leading-tight">
               {movie.title}
             </h3>
-
-            <button className="w-full py-2 px-3 rounded-lg flex items-center justify-center gap-2 text-xs font-semibold transition-colors bg-white/20 backdrop-blur text-white hover:bg-white/30">
-              <Plus size={14} /> Watch Later
-            </button>
           </div>
         </div>
 
         {/* Static Rating Badge (visible when not hovering) */}
-        <div className="absolute top-2 right-2 bg-black/60 backdrop-blur-sm px-2 py-1 rounded-md flex items-center gap-1 group-hover:opacity-0 transition-opacity">
+        <div className="absolute top-2 right-2 bg-black/60 backdrop-blur-sm px-2 py-1 rounded-md flex items-center gap-1">
           <Star size={10} className="text-yellow-400 fill-yellow-400" />
           <span className="text-xs font-medium text-white">{movie.vote_average.toFixed(1)}</span>
         </div>
