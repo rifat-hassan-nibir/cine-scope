7c63ec1f1620c155292af416299cb48677eb6de9
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-06 17:28:45 +0600

    refactor: seperate the fetch options and store it in a variable

--- services/tmdb.ts
+++ services/tmdb.ts
@@ -8,15 +8,20 @@ import {
   MovieSortOption,
 } from "@/types/tmdb";
 
+const getOptions = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+  },
+};
+
 // get top rated movies
 export const getTopRatedMovies = async (): Promise<Movie[]> => {
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/top_rated?language=en-US&page=1`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(
+    `${process.env.TMDB_BASE_URL}/movie/top_rated?language=en-US&page=1`,
+    getOptions,
+  );
   const data: MovieResponse = await res.json();
   return data.results;
 };
@@ -39,65 +44,41 @@ export const getMoviesData = async (
     params.append("with_genres", genreId.toString());
   }
 
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/discover/movie?${params.toString()}`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(
+    `${process.env.TMDB_BASE_URL}/discover/movie?${params.toString()}`,
+    getOptions,
+  );
   const data: MovieResponse = await res.json();
   return data.results || [];
 };
 
 // get all genre id
 export const getAllGenres = async (): Promise<Genre[]> => {
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/genre/movie/list?language=en-US`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(
+    `${process.env.TMDB_BASE_URL}/genre/movie/list?language=en-US`,
+    getOptions,
+  );
   const data: GenreResponse = await res.json();
   return data.genres;
 };
 
 // get movie details
 export const getMovieDetails = async (movieId: number): Promise<MovieDetails> => {
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}`, getOptions);
   const data: MovieDetails = await res.json();
   return data;
 };
 
 // get movie cast details
 export const getMovieCastDetails = async (movieId: number): Promise<CastDetails> => {
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}/credits`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}/credits`, getOptions);
   const data: CastDetails = await res.json();
   return data;
 };
 
 // get similar movies
 export const getSimilarMovies = async (movieId: number): Promise<Movie[]> => {
-  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}/similar`, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  });
+  const res = await fetch(`${process.env.TMDB_BASE_URL}/movie/${movieId}/similar`, getOptions);
   const data: MovieResponse = await res.json();
   return data.results || [];
 };
