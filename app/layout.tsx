ca2e46388b0e73c556e6400b19b44e7bd27f4b21
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2026-02-08 09:49:28 +0600

    feat: add mobile navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import DesktopNav from "@/components/navigation/DesktopNav";
+import MobileNavbar from "@/components/navigation/MobileNavbar";
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
@@ -28,11 +29,11 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${outfit.variable} font-sans antialiased bg-gray-900 text-white`}
       >
-        <div className="flex">
-          {/* desktop nav */}
+        <div className="flex flex-col md:flex-row min-h-screen overflow-x-hidden">
+          <MobileNavbar />
           <DesktopNav />
-          <main className="h-screen flex-1 overflow-y-auto">
-            <div className="max-w-7xl mx-auto p-8">{children}</div>
+          <main className="flex-1 pt-16 md:pt-0 md:h-screen md:overflow-y-auto">
+            <div className="max-w-7xl mx-auto p-4 md:p-8">{children}</div>
           </main>
         </div>
       </body>
